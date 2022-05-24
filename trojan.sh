75d2be85b67830a546a1fda7c4a94a31e87a75f3
ttt789-ppo <72877282+ttt789-ppo@users.noreply.github.com>
2022-05-24 13:12:04 +0800

    Update trojan.sh

--- trojan.sh
+++ trojan.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -f web config.json
-wget -N https://raw.githubusercontent.com/bhqz/Alwaysdata-vmess-vless-trojan-/main/web
+wget -N https://github.com/ttt789-ppo/Alwaysdata-vmess-vless-trojan-1/edit/main/web
 chmod +x ./web
 
 if [[ -z $id ]]; then
